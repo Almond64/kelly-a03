bdc838bd7fb75c54167f969718993eef379880a5
Almond64 <almond64@knights.ucf.edu>
2021-09-30 21:01:23 -0400

    Solution 25 complete

--- src/test/java/baseline/Solution25Test.java
+++ src/test/java/baseline/Solution25Test.java
@@ -6,7 +6,57 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Solution25Test {
     @Test
-    void passwordValidator() {
-        
+    void passwordValidator1() {
+        Solution25 app = new Solution25();
+        String password1 = "12345";
+
+        int actual_result = app.passwordValidator(password1);
+        int expected_result = 0;
+
+        assertEquals(expected_result, actual_result);
+    }
+
+    @Test
+    void passwordValidator2() {
+        Solution25 app = new Solution25();
+        String password2 = "abcdefg";
+
+        int actual_result = app.passwordValidator(password2);
+        int expected_result = 1;
+
+        assertEquals(expected_result, actual_result);
+    }
+
+    @Test
+    void passwordValidator3() {
+        Solution25 app = new Solution25();
+        String password3 = "password3";
+
+        int actual_result = app.passwordValidator(password3);
+        int expected_result = 2;
+
+        assertEquals(expected_result, actual_result);
+    }
+
+    @Test
+    void passwordValidator4() {
+        Solution25 app = new Solution25();
+        String password4 = "p@$$word4";
+
+        int actual_result = app.passwordValidator(password4);
+        int expected_result = 3;
+
+        assertEquals(expected_result, actual_result);
+    }
+
+    @Test
+    void passwordValidator5() {
+        Solution25 app = new Solution25();
+        String password5 = "password";
+
+        int actual_result = app.passwordValidator(password5);
+        int expected_result = 4;
+
+        assertEquals(expected_result, actual_result);
     }
 }

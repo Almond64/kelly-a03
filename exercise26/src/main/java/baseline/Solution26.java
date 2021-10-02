503e551c55366b47f5160fb5c2eeb0a655b35eda
Almond64 <almond64@knights.ucf.edu>
2021-10-01 23:58:19 -0400

    almost there

--- exercise26/src/main/java/baseline/Solution26.java
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -5,22 +5,46 @@
 
 package baseline;
 
+import static java.lang.Math.log;
+import java.util.Scanner;
+
 public class Solution26 {
 
-    private String userInput() {
+    public static final Scanner input = new Scanner(System.in);
+
+    private int userInputBalance() {
+        //Scan user balance
+        System.out.print("What is your balance? ");
+        return input.nextInt();
+        //Scan user APR
+        //Scan user monthly payment
+    }
+
+    private int userInputMonthlyPayment() {
+        //Scan user balance
+        //Scan user APR
+        System.out.print("What is your APR on the card (as a percent)? ");
+        return input.nextInt();
+        //Scan user monthly payment
+    }
+
+    private int userInputAPR() {
         //Scan user balance
         //Scan user APR
         //Scan user monthly payment
-        return null;
+        System.out.print("What is the monthly payment you can make? ");
+        return input.nextInt();
+
     }
 
     public int monthsToPayOffCreditCard(int balance, int APR, int monthlyPayment) {
         //Use the formula to calculate the number of months to pay off a credit card.
-        return 0;
+        return (int) (-(1/30) * log(1 + balance/monthlyPayment *(1 - (1 + APR)^30)) / log(1 + APR));
     }
 
     private void outputStatement(int monthsToPayOffCreditCard) {
         //Output the number of months it will take for the user to pay off their credit card.
+        System.out.printf("It will take %d months to pay off this card.\n", monthsToPayOffCreditCard);
     }
 
     public static void main(String[] args) {

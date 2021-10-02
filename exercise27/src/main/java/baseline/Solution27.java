a40d9b85aed35f54e1422e37cd715d6ed1657aa3
Almond64 <almond64@knights.ucf.edu>
2021-10-02 18:15:20 -0400

    pseudocode

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,83 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Paul Kelly
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution27 {
+
+    public static final Scanner input = new Scanner(System.in);
+
+    private String getFirstName() {
+        //Ask the user to enter their first name
+        //Scan first name
+        return null;
+    }
+
+    private String getLastName() {
+        //Ask the user to enter their last name
+        //Scan last name
+        return null;
+    }
+
+    private String getEmployeeID() {
+        //Ask the user to enter their employee ID
+        //Scan employee ID
+        return null;
+    }
+
+    private int getZipCode() {
+        //Ask the user to enter their zip code
+        //Scan zip code
+        return 0;
+    }
+
+    private String validateFirstName(String getFirstName) {
+        /*
+        Check if the first name is at least two characters long.
+        If not, create string = "The first name must at least be 2 characters long."
+        Check if the first name is filled in.
+        If not, create string = "The first name must be filled in."
+         */
+        return null;
+    }
+
+    private String validateLastName(String getLastName) {
+        /*
+        Check if the last name is at least two characters long.
+        If not, create string = "The last name must at least be 2 characters long."
+        Check if the last name is filled in.
+        If not, create string = "The last name must be filled in."
+         */
+        return null;
+    }
+
+    private String validateEmployeeID(String getEmployeeID) {
+        /*
+        Check if the employee ID is in the format of AA-1234.
+        If not, create string = "The employee ID must be in the form of AA-1234."
+         */
+        return null;
+    }
+
+    private String validateZipCode(int getZipCode) {
+        /*
+        Check if the zip code is a 5 digit integer.
+        If not, create string = "The zipcode must be a 5 digit number."
+         */
+        return null;
+    }
+
+    private void validateInput(String validateFirstName, String validateLastName, String validateEmployeeID, String validateZipCode)  {
+        //Piece together all the outputs from the validation functions into a single output statement.
+    }
+
+    public static void main(String[] args) {
+        //Call all the getter functions
+        //Call all the validation functions
+        //Call validateInput
+    }
+}

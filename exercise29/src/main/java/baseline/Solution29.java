ddcc210965cd02cf65856ac0f821e4f0217e7587
Almond64 <almond64@knights.ucf.edu>
2021-10-03 02:05:55 -0400

    pseudocode

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Paul Kelly
+ */
+
+package baseline;
+
+public class Solution29 {
+
+    private int userInput() {
+        //prompt the user for the rate of return
+        //scan and return the rate of return
+        return 0;
+    }
+
+    public void doubleYourInvestment(int userInput) {
+        //While the input is invalid, say "Sorry. That's not a valid input."
+        //If the input's valid, calculate the time it will take to double the user's investment
+    }
+
+    public static void main(String[] args) {
+        //declare int rate and set equal to the user input
+        //call the doubleYourInvestment function
+    }
+}

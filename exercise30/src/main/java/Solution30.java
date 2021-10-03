abcfa87138375d469e7748487b11f0dd2547f53a
Almond64 <almond64@knights.ucf.edu>
2021-10-03 15:03:06 -0400

    pseudocode

---
+++ exercise30/src/main/java/Solution30.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Paul Kelly
+ */
+
+public class Solution30 {
+
+    public static void main(String[] args) {
+        //declare 12x12 2D int array
+        //int i = 0, j = 0;
+        //for i=0 to i<12
+            //for j=0 to j=12
+                //arr[i][j] = i * j;
+        //print the array with approximately 3 spaces to the right of each number
+    }
+
+}

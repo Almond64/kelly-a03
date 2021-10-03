868262ee3b2142b14471c50e742877f680aea56c
Almond64 <almond64@knights.ucf.edu>
2021-10-03 00:50:46 -0400

    pseudocode

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Paul Kelly
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution28 {
+
+    public static final Scanner input = new Scanner(System.in);
+
+    private int addNumbers() {
+        //for i=0 to i<5, prompt the user to enter a number, scan user input, and add input to total
+        //after loop has executed, return total
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}

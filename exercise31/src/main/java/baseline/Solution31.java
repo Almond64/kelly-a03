0a805716c7e452b764ed4ba5bc6c5a132e650686
Almond64 <almond64@knights.ucf.edu>
2021-10-03 19:22:12 -0400

    Solution 31 complete

--- exercise31/src/main/java/baseline/Solution31.java
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -5,26 +5,51 @@
 
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution31 {
 
-    private int userAge() {
+    public static final Scanner input = new Scanner(System.in);
+
+    private static int userAge() {
         //Prompt user for their age
-        return 0;
+        System.out.print("Enter your age: ");
+        //Scan user age
+        return input.nextInt();
     }
 
-    private int userHeartRate() {
+    private static int userHeartRate() {
         //prompt user for their heart rate
-        return 0;
+        System.out.print("Enter your resting heart rate: ");
+        //Scan user heart rate
+        return input.nextInt();
     }
 
-    public void karvonenHeartRate(int userAge, int userHeartRate) {
+    public static void karvonenHeartRate(int userAge, int userHeartRate) {
         //Output the user's resting pulse and age followed by a newline
+        System.out.printf("Resting Pulse: %d        Age: %d\n", userHeartRate, userAge);
         /*
         Generate a table with Intensity on the left and Rate on the right.
         Intensity ranges from 55% to 95%
         Use the Karvonen Heart Rate formula to determine the target heart rate.
         Generate the table using a for loop that increments intensity by 5%
+        And don't forget to code the formula
         */
+        System.out.println("Intensity     | Rate\n------------|--------");
+        for (int i = 55; i < 96; i += 5) {
+            System.out.print(i + "%" + "        | "
+                    + ((((220 - userAge) - userHeartRate) * i)
+                    + userHeartRate) + " bpm\n");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        //Call getter functions
+        int age = userAge();
+        int rate = userHeartRate();
+        //calculate Karvonen Heart Rate
+        karvonenHeartRate(age, rate);
     }
 
 }

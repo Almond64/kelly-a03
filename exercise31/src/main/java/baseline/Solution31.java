d347101ffb47e3dd6f75db4d21bb4ab29a29b07c
Almond64 <almond64@knights.ucf.edu>
2021-10-03 17:41:52 -0400

    pseudocode

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,30 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 paul kelly
+ */
+
+package baseline;
+
+public class Solution31 {
+
+    private int userAge() {
+        //Prompt user for their age
+        return 0;
+    }
+
+    private int userHeartRate() {
+        //prompt user for their heart rate
+        return 0;
+    }
+
+    public void karvonenHeartRate(int userAge, int userHeartRate) {
+        //Output the user's resting pulse and age followed by a newline
+        /*
+        Generate a table with Intensity on the left and Rate on the right.
+        Intensity ranges from 55% to 95%
+        Use the Karvonen Heart Rate formula to determine the target heart rate.
+        Generate the table using a for loop that increments intensity by 5%
+        */
+    }
+
+}

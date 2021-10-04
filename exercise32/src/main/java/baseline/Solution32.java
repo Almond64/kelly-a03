3ea5634b8af1aaace2a67df2903e04e77463eacc
Almond64 <almond64@knights.ucf.edu>
2021-10-03 20:03:14 -0400

    pseudocode

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,27 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 paul kelly
+ */
+
+package baseline;
+
+public class Solution32 {
+
+    private int difficultyLevel() {
+        //prompt the user to enter difficulty level (1, 2, or 3)
+        //Scan user input
+        return 0;
+    }
+
+    private void guessingGame(int difficultyLevel) {
+        //If the difficulty level is 1, have the user guess a number between 1 and 10.
+        //If the difficulty level is 2, have the user guess a number between 1 and 100.
+        //If the difficulty level is 3, have the user guess a number between 1 and 1000.
+        //Have the computer pick a random number depending on the difficulty level.
+        //Prompt the user to guess the random number.
+        //If the user guesses incorrectly, prompt the user to guess again
+        //If the user guesses correctly, display the number of guesses it took.
+        //Finally, ask the user if they'd like to play again
+    }
+
+}

d2c3a2b68c3e20825f1e2ebe4854cb6b2197986d
Almond64 <almond64@knights.ucf.edu>
2021-10-03 22:27:26 -0400

    Solution 32 complete

--- exercise32/src/main/java/baseline/Solution32.java
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -5,15 +5,25 @@
 
 package baseline;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class Solution32 {
 
-    private int difficultyLevel() {
+    public static final Scanner input = new Scanner(System.in);
+
+    private static int difficultyLevel() {
         //prompt the user to enter difficulty level (1, 2, or 3)
+        System.out.print("Enter the difficulty level (1, 2, or 3): ");
         //Scan user input
-        return 0;
+        return input.nextInt();
     }
 
-    private void guessingGame(int difficultyLevel) {
+    private static void guessingGame(int difficultyLevel) {
+        Random rand = new Random();
+        int guess_number;
+        int user_guess;
+        int guessCount = 0;
         //If the difficulty level is 1, have the user guess a number between 1 and 10.
         //If the difficulty level is 2, have the user guess a number between 1 and 100.
         //If the difficulty level is 3, have the user guess a number between 1 and 1000.
@@ -21,7 +31,99 @@ public class Solution32 {
         //Prompt the user to guess the random number.
         //If the user guesses incorrectly, prompt the user to guess again
         //If the user guesses correctly, display the number of guesses it took.
-        //Finally, ask the user if they'd like to play again
+        if (difficultyLevel == 1) {
+            //generate random number between 1 and 10
+            guess_number = rand.nextInt(10) + 1;
+            //prompt user for guess
+            System.out.print("I have my number. What's your guess? ");
+            //Scan user guess
+            user_guess = input.nextInt();
+            //while user guesses incorrectly, prompt them to guess again
+            while (user_guess != guess_number) {
+                if (user_guess < guess_number) {
+                    System.out.print("Too low. Guess again: ");
+                    user_guess = input.nextInt();
+                    guessCount++;
+                }
+                if (user_guess > guess_number) {
+                    System.out.print("Too high. Guess again: ");
+                    user_guess = input.nextInt();
+                    guessCount++;
+                } else {
+                    System.out.printf("You got it in %d guesses!\n", guessCount);
+                    break;
+                }
+            }
+        } else if (difficultyLevel == 2) {
+            //generate random number between 1 and 100
+            guess_number = rand.nextInt(100) + 1;
+            //prompt user for guess
+            System.out.print("I have my number. What's your guess? ");
+            //Scan user guess
+            user_guess = input.nextInt();
+            //while user guesses incorrectly, prompt them to guess again
+            while (user_guess != guess_number) {
+                if (user_guess < guess_number) {
+                    System.out.print("Too low. Guess again: ");
+                    user_guess = input.nextInt();
+                    guessCount++;
+                }
+                if (user_guess > guess_number) {
+                    System.out.print("Too high. Guess again: ");
+                    user_guess = input.nextInt();
+                    guessCount++;
+                } else {
+                    System.out.printf("You got it in %d guesses!\n", guessCount);
+                    break;
+                }
+            }
+        } else {
+            //generate random number between 1 and 1000
+            guess_number = rand.nextInt(1000) + 1;
+            //prompt user for guess
+            System.out.print("I have my number. What's your guess? ");
+            //Scan user guess
+            user_guess = input.nextInt();
+            //while user guesses incorrectly, prompt them to guess again
+            while (user_guess != guess_number) {
+                if (user_guess < guess_number) {
+                    System.out.print("Too low. Guess again: ");
+                    user_guess = input.nextInt();
+                    guessCount++;
+                }
+                if (user_guess > guess_number) {
+                    System.out.print("Too high. Guess again: ");
+                    user_guess = input.nextInt();
+                    guessCount++;
+                } else {
+                    System.out.printf("You got it in %d guesses!\n", guessCount);
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void playAgain() {
+        int level;
+        //prompt the user if they wish to play again
+        System.out.print("Do you wish to play again (Y/N)? ");
+        String answer = input.next();
+        //if the user says yes, call difficulty level
+        //else, quit
+        if (answer.equals("y")) {
+            level = difficultyLevel();
+            guessingGame(level);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Let's play Guess the Number!\n");
+        //call difficulty level function
+        int level = difficultyLevel();
+        //call guessing game function
+        guessingGame(level);
+        //call play again function
+        playAgain();
     }
 
 }
